fe37a8da2bf0f2024263113096b1b526facfe3df
Attila <attila@p4d.co.uk>
2020-02-04 21:02:50 +0000

    preparing game controller

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Games;
+
+
 class GameController extends Controller
 {
-    //
+    public function index() {
+
+    	$all_games = array();
+
+    	//get all games from the DB
+    	$all_games = Games::all();
+    	
+    	return view('welcome', compact('all_games'));
+    }
 }
