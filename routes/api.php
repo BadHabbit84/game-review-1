de3add483335d5572040533e49d57c373c6ec481
Attila <attila@p4d.co.uk>
2020-02-05 21:38:03 +0000

    added API endpoints

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('get-all-games', 'APIController@getAllGames');
+Route::post('get-games', 'APIController@getGames');
+Route::post('get-users', 'APIController@getUsers');
+Route::post('get-comments/{user_name}', 'APIController@getComments');
